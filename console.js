b359f8eab67f7860b6196f93d722af433df12cb2
Ujjwal Puri <133661992+UzitheI@users.noreply.github.com>
2023-06-20 08:39:20 +0545

    Add files via upload
    jun 20(basics)

---
+++ console.js
@@ -0,0 +1,10 @@
+//console is a built in environment provided by web browsers it can be/*it can be used to test javascript codes inside the webpage
+/*it can be accesed using Ctrl+Shift and J
+the console is a REPL enviroment which means Read evaluate print and loop
+it creates an insitu environment which makes it possible to test arbitary codes in an instant which makes it possible to work with js codes without having to create seperate files
+now we will learn about console.log();
+console.log is equivalent to document.write
+console.log helps in logging the file inside the console
+when we type 5+4 in the console the output is given as 9 but when we use console.log(5+4); we get the value 9 and also undefined 
+undefined is a special value that is present in js which means the return value of console.log turns out to be undefined
+ */
---
+++ intorduction.js
@@ -0,0 +1,13 @@
+//javascript is the most used programming language and even if it sounds similar to java it's function and origin is completely different from one another
+/*javascript can do multiple things but mostly it is used to design websites and web applications */
+document.getElementById("workout").innerHTML = "dont workout";
+//this searches the word 'workout at first and then converts that into dont workout
+
+/*javascript is also capable of changing the value of html attritubes 
+they can help change the css values of a program
+it can show html elements, it can hide html elements*/
+//javascript like python sits inside and engine, so it is an interpreted language, unlike c and c++ which are used with a compiler
+// the most popular implementation(engine) of javascript is V8 made by google 
+//node.js is a server side run time environment
+// angular vue and svelte are a few examples of frameworks of javascript
+ 
